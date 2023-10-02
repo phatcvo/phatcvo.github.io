edeee8130843701c5b11f981d5b2e664d2720b46
Phat C. Vo <vophat0607@gmail.com>
2023-10-02 15:17:21 +0900

    fix

--- public/css/js/custom.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    renderMathInElement(document.body, {
-        delimiters: [
-            { left: "$$", right: "$$", display: true },
-                { left: "\\[", right: "\\]", display: true },
-                { left: "\begin{bmatrix}", right: "\end{bmatrix}", display: true },
-        ],
-    });
-});
